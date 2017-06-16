a83113ed468d18e4b643127bc43b03d957ab4000
Erica Y Stevens <ericastevens99@gmail.com>
2017-06-16 13:11:50 -0400

    Added NYTMovieReviews Exercise Project

---
+++ AC-iOS-NYTMovieReviews/AC-iOS-NYTMovieReviews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AC-iOS-NYTMovieReviews
+//
+//  Created by Erica Y Stevens on 6/16/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
